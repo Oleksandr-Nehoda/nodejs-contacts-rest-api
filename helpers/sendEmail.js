39b49c26db6e05705d284a69a497634924507816
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-04-06 01:11:53 +0300

    added helpers sendEmail and properties userModel

---
+++ helpers/sendEmail.js
@@ -0,0 +1,17 @@
+const sgEmail = require('@sendgrid/mail');
+
+const {SENDGRID_API_KEY} = process.env;
+
+sgEmail.setApiKey(SENDGRID_API_KEY);
+
+const sendEmail = async (data) => {
+    try {
+        const email = {...data, from: "iamsoul@ukr.net"};
+        await sgEmail.send(email);
+        return true;
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = sendEmail;
